5e7f381b8f5bce9f6d6bc37b5b840c4cd0958611
The-Aman-Sharma <aman3374sharma@gmail.com>
2024-06-07 17:48:33 +0530

    can be added to portfolio

--- sportify-playlist-gen/src/components/Header/Header.js
+++ sportify-playlist-gen/src/components/Header/Header.js
@@ -1,27 +1,50 @@
-import React from 'react';
-import Button from '../Button/Button.js'
+import React, { useState } from 'react';
+import Button from '../Button/Button.js';
 import './Header.css';
+import mainLogo from '../../BoilerPlates/images/logo/temp.png';
+import { IoMenuSharp } from "react-icons/io5";
 
 function Header() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const expandContainer = () => {
+    setMenuVisible(prev => !prev);
+  };
+
   return (
     <div id="header">
+      <div className="mainHeader">
+        <img src={mainLogo} alt="logo" className='logo' />
 
-    <img src="" alt="" srcset="" />
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Playlist Generator</a></li>
+          <li><a href="">Contact</a></li>
+          
+        </ul>
 
+        <Button name={"Sign up"} />
+      </div>
 
-    <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">Playlist Generator</a></li>
-        <li><a href="">Contact</a></li>
+      <div className="mobileMenu">
+        <div className="blankMenu">
+          <img src={mainLogo} alt="logo" className='logo' />
+          <IoMenuSharp color='3cb428' onClick={expandContainer} />
+        </div>
 
-    </ul>
-
-
-    <Button name={"Sign up"} ></Button>
-
-</div> 
-
-  )
+        {menuVisible && (
+          <div className="menuExpanded" id='menuExpanded'>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Playlist Generator</a></li>
+              <li><a href="">Contact</a></li>
+              <li><a href="">Sign Up</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
